4f92226621b7b484f6bab694cc3324a549876d32
DMCMAYCYR <62834243+DMCMAYCYR@users.noreply.github.com>
2021-03-02 17:25:16 +0300

    Deneme2
    Bistemis

---
+++ Dene2D/Assets/scipirt/nextLevel.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class nextLevel : MonoBehaviour
+{
+    public void OnTriggerStay2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
